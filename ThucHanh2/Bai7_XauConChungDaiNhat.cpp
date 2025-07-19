36f84d662260f26c1b0ba9a6ad450d1bf8cba58b
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-07-19 15:35:13 +0700

    19/07/2025

--- ThucHanh2/Bai7_XauConChungDaiNhat.cpp
+++ ThucHanh2/Bai7_XauConChungDaiNhat.cpp
@@ -32,7 +32,6 @@ int main() {
 		b = " " + b;
 		int dp[n + 1][m + 1] = {};
 		for (int i=1; i<=n; i++) {
-			
 			for (int j=1; j<=m; j++) {
 				if (a[i] == b[j])
 					dp[i][j] = dp[i - 1][j - 1] + 1;
