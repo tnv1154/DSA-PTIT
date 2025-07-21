cd16baf18a00f165bedcfc1f14b740deca457320
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-07-21 23:58:41 +0700

    21/07/2025

--- DSA02001_DaySo1.cpp
+++ DSA02001_DaySo1.cpp
@@ -1,11 +1,28 @@
-//
-// Created by ngvie on 7/18/2025.
-//
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
+        for (int step = 0; step < n; step++) {
+            cout << "[";
+            for (int i = 0; i < n - step; i++) {
+                cout << a[i];
+                if (i < n - step - 1) cout << " ";
+            }
+            cout << "]" << endl;
+
+            for (int i = 0; i < n - step - 1; i++) {
+                a[i] = a[i] + a[i + 1]; // Tính tổng cặp
+            }
+        }
+    }
 
     return 0;
 }
