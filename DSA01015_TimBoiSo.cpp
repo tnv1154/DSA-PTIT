71186a050cb99cab90949a0a1c94a9b326ec7af7
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-08-03 15:04:06 +0700

    03/08/2025

--- DSA01015_TimBoiSo.cpp
+++ DSA01015_TimBoiSo.cpp
@@ -1,3 +1,36 @@
 //
 // Created by ngvie on 8/3/2025.
 //
+#include<bits/stdc++.h>
+using namespace std;
+
+string boiSo(int n) {
+    queue<string> q;
+    q.push("9");
+    while (q.size()) {
+        string tmp = q.front();
+        q.pop();
+        if (stoll(tmp) % n == 0)
+            return tmp;
+
+        q.push(tmp + "0");
+        q.push(tmp + "9");
+    }
+    return "-1";
+
+}
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << boiSo(n) << endl;
+
+    }
+
+    return 0;
+}
