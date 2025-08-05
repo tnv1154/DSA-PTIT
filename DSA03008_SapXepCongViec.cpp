6bcf03f34ede7a56abb0ab3df62e6ce5af227e75
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-08-05 16:59:34 +0700

    05/08/2025

--- DSA03008_SapXepCongViec.cpp
+++ DSA03008_SapXepCongViec.cpp
@@ -1,3 +1,36 @@
 //
 // Created by ngvie on 8/5/2025.
 //
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<pair<int, int>> v(n);
+        for (int i=0; i<n; i++) {
+            cin >> v[i].second;
+        }
+        for (int i=0; i<n; i++)
+            cin >> v[i].first;
+        sort(v.begin(), v.end());
+        int cnt = 1;
+        int tmp = v[0].first;
+        for (int i=1; i<n; i++) {
+            if (v[i].second >= tmp) {
+                tmp = v[i].first;
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+
+
+
+    }
+
+
+    return 0;
+}
---
+++ DSA03010_NoiDay1.cpp
@@ -0,0 +1,3 @@
+//
+// Created by ngvie on 8/5/2025.
+//
