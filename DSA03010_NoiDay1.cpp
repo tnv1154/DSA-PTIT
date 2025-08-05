df61c7df665dd3df91b5e6410d0df4eb6d8aa08a
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-08-05 21:29:09 +0700

    05/08/2025

--- DSA03010_NoiDay1.cpp
+++ DSA03010_NoiDay1.cpp
@@ -1,3 +1,33 @@
 //
 // Created by ngvie on 8/5/2025.
 //
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        priority_queue<long long, vector<long long>, greater<long long>> q;
+        for (int i=0; i<n; i++) {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        long long sum = 0;
+        while (q.size() > 1) {
+            long long x = q.top();
+            q.pop();
+            long long y = q.top();
+            q.pop();
+            long long s = x + y;
+            q.push(s);
+            sum += s;
+        }
+        cout << sum << endl;
+    }
+
+    return 0;
+}
