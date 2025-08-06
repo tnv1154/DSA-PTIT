d2372f4d3c6bece052e116ae79d58ad9be6216fb
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-08-06 23:04:20 +0700

    05/08/2025

---
+++ DSA07004_DemSoDauNgoacDoiChieu.cpp
@@ -0,0 +1,18 @@
+//
+// Created by ngvie on 8/6/2025.
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+
+
+
+    }
+
+    return 0;
+}
