15d91c0b7a53254c4d4f3fc447c7c3ca736b57fa
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-08-07 21:58:24 +0700

    07/08/2025

--- DSA07009_BienDoiTienTo_TrungTo.cpp
+++ DSA07009_BienDoiTienTo_TrungTo.cpp
@@ -1,4 +1,33 @@
 //
 // Created by ngvie on 8/7/2025.
 //
-#include<>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        stack<string> st;
+        int n = s.length();
+        for (int i = n - 1; i>=0; i--) {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                st.push(string(1, s[i]));
+            else {
+                string a = st.top();
+                st.pop();
+                string b = st.top();
+                st.pop();
+                string c = "(" + a + string(1, s[i]) + b + ")";
+                st.push(c);
+            }
+        }
+        cout << st.top() << endl;
+
+    }
+
+    return 0;
+}
